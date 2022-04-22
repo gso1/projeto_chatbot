90c6a7ea5e6ec0581a4a70207196d9264ce7a175
Pedro Cunha <pvcunh4@gmail.com>
2022-04-22 12:12:16 -0300

    rdt 3.0

--- rdt.py
+++ rdt.py
@@ -2,14 +2,13 @@ import socket
 from random import random
 
 class rdt_connection:
-    
-    seq_num = 1
 
     def __init__(self, port, ip='localhost', type='client', buffer_size=248):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_addr = (ip, port)
         self.buffer_size = buffer_size
         self.type = type
+        self.seq_num = 0
         if type == 'server':
             print(f'Server listening on port {port}')
             self.sock.bind(self.server_addr)
@@ -24,8 +23,8 @@ class rdt_connection:
         return self.sock.recvfrom(self.buffer_size)
 
     def rdt_send(self, msg):
-        pkt = self.make_pkt(msg)
-        self.seq_num += 1
+        sum = self.checksum(msg.encode())
+        pkt = self.make_pkt(msg, sum)
         pktsnd = pkt.encode()
         self.sock.settimeout(1)
         self.udt_send(pktsnd)
@@ -34,8 +33,8 @@ class rdt_connection:
         while not ack:
             try:
                 pktrcv, _ = self.rdt_rcv()
-                if not pktrcv['ack']:
-                    self.udt_send(pktsnd)
+                if pktrcv['sum'] != sum or pktrcv['ack'] != self.seq_num:
+                    continue
             except socket.timeout as err:
                 print('timeout error')
                 self.udt_send(pktsnd)
@@ -43,22 +42,32 @@ class rdt_connection:
             else:
                 self.sock.settimeout(None)
                 ack = 1
+        self.seq_num = 1 - self.seq_num
                     
     def rdt_rcv(self, type='sender'):
         bytes, sender_addr = self.udt_rcv()
         pkt = eval(bytes.decode())
-        p = random()
-        corrupt = self.corrupt(pkt)
-        if type == 'receiver' and corrupt:
-            pkt['ack'] = 0
-
+        isvalid = True
+        # p = random()
         if type == 'receiver':
-            self.udt_send(str(pkt).encode(), addr=sender_addr)
-        
-        return pkt, sender_addr
+            sum = self.checksum(pkt['data'].encode())
+            sndack = self.make_ack(sum, ack=self.seq_num)
 
-    def make_pkt(self, msg, ack=1):
-        return str({'ack': ack , 'seq': self.seq_num, 'sum': self.checksum(msg.encode()), 'data': msg})
+            if self.corrupt(pkt) or self.seq_num != pkt['seq']:
+                sndack['ack'] = pkt['seq']
+                isvalid = False
+            else:
+                self.seq_num = 1 - self.seq_num
+            
+            self.udt_send(str(sndack).encode(), addr=sender_addr)
+        
+        return (pkt, sender_addr) if isvalid else (None, None)
+
+    def make_pkt(self, msg, checksum, ack=1):
+        return str({'seq': self.seq_num, 'sum': checksum, 'data': msg})
+
+    def make_ack(self, checksum, ack=1):
+        return {'ack': ack, 'sum': checksum}
 
     def corrupt(self, pkt):
         return pkt['sum'] != self.checksum(pkt['data'].encode())
